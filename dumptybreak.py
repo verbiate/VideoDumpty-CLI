6c0345353b0235f7bd2f6244c0341c13edcf3525
Hans van de Bruggen <hansv@cureatr.com>
2019-09-26 03:18:20 -0700

    Add files via upload

---
+++ dumptybreak.py
@@ -0,0 +1,31 @@
+import subprocess
+import json
+import argparse
+import os
+
+
+###Set some input arguments. https://docs.python.org/3/howto/argparse.html
+
+parser = argparse.ArgumentParser()
+parser.add_argument("inputvideo")
+args = parser.parse_args()
+#To do: handle spaces on filenames better
+usrinputvideo = args.inputvideo
+usrinputvideoname = os.path.splitext(usrinputvideo)[0]
+
+
+###Make a directory to put the frames into based on the name of the input video
+
+if not os.path.exists(".\\frames\\" + usrinputvideoname):
+    os.makedirs(".\\frames\\" + usrinputvideoname)
+
+
+###Gimmie the frames
+
+#Use this line for frames scaled to a maximum width of 200px. Scaled frames 
+#can be easier to use in some cases, such as with machine learning.
+#framebreak=".\\ffmpeg\\ffmpeg.exe -i " + usrinputvideo + " -vf scale='min(200,iw)':-2 .\\frames\\" + usrinputvideoname + "\\" + usrinputvideoname + "-%07d.png"
+
+#Use this line for full-size frames
+framebreak=".\\ffmpeg\\ffmpeg.exe -i " + usrinputvideo + " .\\frames\\" + usrinputvideoname + "\\" + usrinputvideoname + "-%07d.png"
+result = subprocess.check_output(framebreak, shell=True)
