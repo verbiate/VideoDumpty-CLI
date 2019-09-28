f92d8004fe6744cdb1d8bef3da6aec4bb05cc441
Hans van de Bruggen <hansv@cureatr.com>
2019-09-27 18:28:18 -0700

    Added ffmpeg commands as comments for clarity

--- dumptyglue.py
+++ dumptyglue.py
@@ -18,7 +18,9 @@ usrinputframespath = usrinputvideoname
 
 ###Probe the video and parse the output as JSON
 
+#.\ffmpeg\ffprobe.exe -i <<INPUTVIDEOPATH>> -print_format json -loglevel fatal -show_streams -count_frames -select_streams v
 ffprober = ".\\ffmpeg\\ffprobe.exe -i " + usrinputvideo + " -print_format json -loglevel fatal -show_streams -count_frames -select_streams v"
+
 result = subprocess.check_output(ffprober, shell=True)
 ffproberparsed = json.loads(result)
 
@@ -30,12 +32,14 @@ print(framerate)
 
 ###Make a temp video with no audio
 
+#.\ffmpeg\ffmpeg.exe -r <<INPUTVIDEOFPS>> -f image2 -pix_fmt yuv420p -i .\frames\<<INPUTVIDEONAME>>\<<INPUTVIDEONAME>>-%07d.png -vcodec libx264 -crf 17 -pix_fmt yuv420p -y <<INPUTVIDEONAME>>-temp.mp4"
 ffmuted = ".\\ffmpeg\\ffmpeg.exe -r " + framerate + " -f image2 -pix_fmt yuv420p -i .\\frames\\" + usrinputframespath + "\\" + usrinputframespath + "-%07d.png -vcodec libx264 -crf 17 -pix_fmt yuv420p -y " + usrinputvideoname + "-temp.mp4"
 result = subprocess.check_output(ffmuted, shell=True)
 
 
 ###Glue together the audio and the frames at the original frame rate
 
+#.\ffmpeg\ffmpeg.exe -I <<INPUTVIDEONAME>>-temp.mp4 -i <<INPUTVIDEOPATH>> -map 0:v:0? -map 1? -map -1:v? -c copy -pix_fmt yuv420p <<INPUTVIDEONAME>>-glued.mp4 -y
 ffglue = ".\\ffmpeg\\ffmpeg.exe -i " + usrinputvideoname + "-temp.mp4 -i " + usrinputvideo + " -map 0:v:0? -map 1? -map -1:v? -c copy -pix_fmt yuv420p " + usrinputvideoname + "-glued.mp4 -y"
 result = subprocess.check_output(ffglue, shell=True)
 
